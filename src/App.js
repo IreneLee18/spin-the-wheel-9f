d16726704398bc49bcb326b93d5a14fa8781ec34
IreneLee18 <cc86418520@gmail.com>
2022-09-21 20:32:09 +0800

    add wheel data and mack wheel css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import prize from "./Utils/Prize6.json";
+import WheelInner from "./Components/WheelInner";
+import { useState, useEffect, useCallback } from "react";
 function App() {
+  const [startDeg, setStartDeg] = useState(0);
+  const [deg, setDeg] = useState(startDeg);
+  const [start, setStart] = useState(false);
+  const handleClick = () => {
+    setStart(true);
+  };
+  const getDeg = useCallback(() => {
+    const picked = Math.floor(Math.random() * prize.length);
+    const currentDeg = Number(startDeg) + 1800 + picked * 60 - (startDeg % 360)
+    console.log(picked,currentDeg,startDeg % 360)
+    setDeg(currentDeg);
+    setStartDeg(currentDeg);
+  },[startDeg]);
+  useEffect(() => {
+    if (start) {
+      getDeg();
+      setStart(false);
+    }
+  }, [getDeg, start]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="wheel">
+          <div className="wheel-outside">
+            {/* 6個的 */}
+            <WheelInner prize={prize} />
+            <div className="wheel-center" onClick={handleClick}>
+              <div
+                className="wheel-hand"
+                style={{ transform: `rotate(${deg}deg)` }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/Utils/Data.js
@@ -0,0 +1,38 @@
+export  const data = [
+  {
+    id: "Movie",
+    class: "material-symbols-outlined",
+    title: "movie_filter",
+    style: "rotate(-60deg) skewX(-30deg)",
+  },
+  {
+    id: "Wish",
+    class: "material-icons-outlined",
+    title: "cake",
+    style: "rotate(0deg) skewX(-30deg)"
+  },
+  {
+    id: "Anything",
+    class: "material-symbols-outlined",
+    title: "stars",
+    style: "rotate(60deg) skewX(-30deg)"
+  },
+  {
+    id: "Child",
+    class: "material-icons-outlined",
+    title: "child_care",
+    style: "rotate(120deg) skewX(-30deg)"
+  },
+  {
+    id: "Flight",
+    class: "material-symbols-outlined",
+    title: "flight",
+    style: "rotate(180deg) skewX(-30deg)"
+  },
+  {
+    id: "Wifi",
+    class: "material-symbols-outlined",
+    title: "wifi",
+    style: "rotate(240deg) skewX(-30deg)"
+  },
+];
